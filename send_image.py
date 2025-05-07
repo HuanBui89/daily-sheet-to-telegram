5f81c7b2cf38a8a26fdb2434dcf650a7e6af75b3
HuanBui89 <126949720+HuanBui89@users.noreply.github.com>
2025-05-07 13:03:41 +0700

    Create send_image.py

---
+++ send_image.py
@@ -0,0 +1,16 @@
+import requests
+from telegram import Bot
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("GROUP_CHAT_ID")
+
+# ✅ Thay bằng link export Google Sheet dạng PNG (lưu ý file công khai!)
+IMAGE_URL = "IMAGE_URL = "https://docs.google.com/spreadsheets/d/1G7ql9O5J0nMJ9qkiOsadjPYATo3ZhCgXPTAlx8oUo4U/export?format=png&id=1G7ql9O5J0nMJ9qkiOsadjPYATo3ZhCgXPTAlx8oUo4U&gid=0&range=A2:N26"
+
+bot = Bot(token=TELEGRAM_TOKEN)
+print("📤 Đang tải ảnh báo cáo...")
+image_data = requests.get(IMAGE_URL).content
+print("✅ Đang gửi tới Telegram...")
+bot.send_photo(chat_id=CHAT_ID, photo=image_data, caption="📊 Báo cáo số liệu HCM4 sáng nay!")
+print("🎉 Đã gửi thành công.")
